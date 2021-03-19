7381db74b8d3663f41148a21e70e73caa97c97bf
DionatanLeao <dionatan.ti@outlook.com>
2021-03-19 13:13:09 -0300

    implementing findAll and findById method

--- src/main/java/com/keys/stock/controller/KeyController.java
+++ src/main/java/com/keys/stock/controller/KeyController.java
@@ -3,9 +3,13 @@ package com.keys.stock.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.keys.stock.model.entity.Key;
 import com.keys.stock.repository.KeyRepository;
@@ -21,4 +25,12 @@ public class KeyController {
 	public List<Key> findAll() {
 		return keyRepository.findAll();
 	}
+
+	@GetMapping("{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Key findById(@PathVariable Long id) {
+		return keyRepository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
 }
