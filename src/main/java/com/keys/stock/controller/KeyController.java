65e96f687f8ae716eb1687429012a28d12ce71f0
DionatanLeao <dionatan.ti@outlook.com>
2021-03-19 13:14:30 -0300

    implementing save method

--- src/main/java/com/keys/stock/controller/KeyController.java
+++ src/main/java/com/keys/stock/controller/KeyController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,6 +33,12 @@ public class KeyController {
 	public Key findById(@PathVariable Long id) {
 		return keyRepository.findById(id)
 				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Key save(@RequestBody Key key) {
+		return keyRepository.save(key);
 	}
 
 }
