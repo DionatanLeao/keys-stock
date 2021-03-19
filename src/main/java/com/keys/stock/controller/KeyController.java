f166ec0a083f0bb1fcf371cd0c949fdd046e1e0c
DionatanLeao <dionatan.ti@outlook.com>
2021-03-19 13:15:36 -0300

    implementing delete method

--- src/main/java/com/keys/stock/controller/KeyController.java
+++ src/main/java/com/keys/stock/controller/KeyController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,15 @@ public class KeyController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Key save(@RequestBody Key key) {
 		return keyRepository.save(key);
+	}
+	
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		keyRepository.findById(id).map(key -> {
+			keyRepository.delete(key);
+			return Void.TYPE;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
 	}
 
 }
